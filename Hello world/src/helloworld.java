19294328533f72000f80093e547c2f679ba356da
erdelsor <sorooeegii2@gmail.com>
2022-03-29 14:31:21 +0900

    add print line

--- Hello world/src/helloworld.java
+++ Hello world/src/helloworld.java
@@ -3,7 +3,8 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("hello wowrld");
+		System.out.println("Hello his world");
 	}
 
 }
