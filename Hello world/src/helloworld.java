ae5ca412c75727430bffb5c19b0957f4ff3cbbeb
erdelsor <sorooeegii2@gmail.com>
2022-03-31 10:55:13 +0900

    added 'her world'

--- Hello world/src/helloworld.java
+++ Hello world/src/helloworld.java
@@ -1,10 +1,10 @@
-
 public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello wowrld");
 		System.out.println("Hello his world");
+		System.out.println("Hello her world");
 	}
 
 }
